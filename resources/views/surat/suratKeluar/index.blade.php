9911cf87c26f3d7bc8b9d6ae8d166def29cc75fc
rachmadbud <rachmad.16421044@student.ubl.ac.id>
2026-01-07 16:33:53 +0700

    tujuan column skeluar

--- resources/views/surat/suratKeluar/index.blade.php
+++ resources/views/surat/suratKeluar/index.blade.php
@@ -29,7 +29,7 @@
                                         <th>Tanggal</th>
                                         <th>No Surat</th>
                                         <th>Perihal</th>
-                                        <th>Ditujukan</th>
+                                        <th>Tujukan</th>
                                         <th>Lampiran</th>
                                         <th>created_by</th>
                                         <th>Aksi</th>
