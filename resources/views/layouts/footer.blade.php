f03b63a5c7bd6ab463d08565075ebcb5ea20bed4
rachmadbud <rachmad.16421044@student.ubl.ac.id>
2026-01-08 14:47:29 +0700

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <strong>{{ config('app.name') }}</strong>
     <div class="float-right d-none d-sm-inline-block">
-        Grup Teknologi Informasi | {{ date("Y") }}
+        Gapkindo Pusat | {{ date('Y') }}
     </div>
 </footer>
