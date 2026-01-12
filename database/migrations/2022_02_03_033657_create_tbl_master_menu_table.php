aecb9fdf631f5269dee2b39cfef09d98afbeafe2
rachmadbud <rachmad.16421044@student.ubl.ac.id>
2026-01-12 11:26:15 +0700

    surat masuk desc

--- database/migrations/2022_02_03_033657_create_tbl_master_menu_table.php
+++ database/migrations/2022_02_03_033657_create_tbl_master_menu_table.php
@@ -26,6 +26,8 @@ class CreateTblMasterMenuTable extends Migration
         DB::table('tbl_master_menu')->insert(['id' => '2', 'nama' => 'Manajemen', 'link' => '#', 'status_data' => '1', 'urutan' => '2']);
         DB::table('tbl_master_menu')->insert(['id' => '3', 'nama' => 'Konfigurasi', 'link' => '#', 'status_data' => '1', 'urutan' => '3']);
         DB::table('tbl_master_menu')->insert(['id' => '4', 'nama' => 'Surat', 'link' => '#', 'status_data' => '1', 'urutan' => '4']);
+
+        DB::table('tbl_master_menu')->insert(['id' => '5', 'nama' => 'Ekspor & Impor', 'link' => '#', 'status_data' => '1', 'urutan' => '5']);
     }
 
     /**
