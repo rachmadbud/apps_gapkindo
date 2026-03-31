f514cd6b236ef301d8ab481043285cf0e6c9bc5c
rachmadbud <rachmad.16421044@student.ubl.ac.id>
2026-03-31 16:01:24 +0700

    karnel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send:expired-reminder')->dailyAt('03:50');
-        // $schedule->command('send:expired-reminder')->everyMinute();
+        // $schedule->command('send:expired-reminder')->dailyAt('03:50');
+        $schedule->command('send:expired-reminder')->everyMinute();
     }
 
 
