1b72fde09a3b8a9c8cfb836ef60f511fd3d8718e
rachmadbud <rachmad.16421044@student.ubl.ac.id>
2026-07-02 14:19:10 +0700

    tanda surat masuk fix

--- resources/views/surat/suratMasuk/print.bladea4-2.php
+++ resources/views/surat/suratMasuk/print.bladea4-2.php
@@ -8,14 +8,14 @@
     <style>
         @page {
             /* Kertas disetel ke A4 Portrait */
-            size: A4 landscape;
+            size: A4 portrait;
             /* WAJIB: hilangkan margin agar konten bisa melebar penuh 21cm tanpa kegencet */
             margin: 0;
         }
 
         body {
             font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-            font-size: 11px;
+            font-size: 13px;
             line-height: 1.4;
             margin: 0;
             padding: 0;
@@ -27,26 +27,24 @@
 
         /* AREA SETENGAH KERTAS A4 (A5 LANDSCAPE ATAS) */
         .page-half-a4 {
-            width: 50%;
-            height: 18.85cm;
+            width: 100%;
+            height: 14.85cm;
             /* Pas setengah dari tinggi A4 (29.7cm) */
             position: relative;
             box-sizing: border-box;
             /* Beri padding agar isi form tidak terlalu rapat dengan pinggiran potongan kertas */
             padding: 0.6cm 0.8cm 0 0.8cm;
             overflow: hidden;
-
-            margin-left: auto;
         }
 
         /* BORDER UTAMA FORM */
         .form-container {
             width: 100%;
-            height: auto;
+            height: 13.2cm;
             /* Dikunci agar pas menyisakan ruang untuk teks sistem di bawah */
             box-sizing: border-box;
             position: relative;
-            border: 3px solid #000;
+            border: 1px solid #000;
             /* Kotak hitam yang membungkus form */
         }
 
@@ -58,20 +56,20 @@
 
         /* Header area */
         .header-box {
-            border: 1px solid #000;
+            border: 2px solid #000;
             background-color: #f2f2f2;
-            padding: 3px 5px;
+            padding: 6px 10px;
         }
 
         .header-box h2 {
-            font-size: 13px;
+            font-size: 15px;
             font-weight: bold;
             margin: 0;
             text-transform: uppercase;
         }
 
         .header-box p {
-            font-size: 11px;
+            font-size: 12px;
             margin: 2px 0 0 0;
         }
 
@@ -105,7 +103,7 @@
         /* Bagian Perihal */
         .perihal-section {
             text-align: center;
-            margin-bottom: 0;
+            margin-bottom: 8px;
         }
 
         .perihal-title {
@@ -116,24 +114,23 @@
         }
 
         .perihal-box {
-            border: 1px solid;
+            border: 1px solid #000;
             background-color: #f2f2f2;
-            padding: 9px;
+            padding: 6px;
             font-weight: bold;
             font-size: 13px;
-            height: 2.5cm;
         }
 
         /* === TABEL DISPOSISI BAWAH === */
         .tabel-disposisi {
             width: 100%;
             border-collapse: collapse;
-            border: 1px solid #000;
+            border: 2px solid #000;
             margin-bottom: 0;
         }
 
         .tabel-disposisi td {
-            padding: 15px 8px;
+            padding: 5px 8px;
             vertical-align: middle;
         }
 
@@ -142,8 +139,8 @@
         }
 
         .bg-dark {
-            background-color: #e7e4e4;
-            color: #161616;
+            background-color: #333;
+            color: #fff;
             font-weight: bold;
             text-align: center;
         }
@@ -160,17 +157,9 @@
 
         /* KOTAK CATATAN DISET LUAS DAN LEGA */
         .row-catatan td {
-            padding: 0 !important;
-            vertical-align: top;
-            border: 1px solid #000 !important;
-        }
-
-        /* Kita tembak langsung tag div di dalam selnya */
-        .row-catatan .space-catatan {
-            height: 13.5cm !important;
-            /* Paksa div ini melar setinggi 3.5cm */
-            width: 100%;
-            display: block;
+            height: 3.4cm !important;
+            /* Membuat area kosong catatan melar ke bawah */
+            border-bottom: none !important;
         }
 
         /* TEKS FOOTER SISTEM (Menempel pas di bawah kotak border form) */
@@ -183,7 +172,7 @@
             justify-content: space-between;
             font-size: 10px;
             color: #000;
-            margin: 1%;
+            margin: 0;
         }
     </style>
 </head>
@@ -199,43 +188,30 @@
                 <p>Lembar Bukti Agenda Surat Masuk</p>
             </div>
 
-            {{-- <div class="status-bar" style="display: flex; flex-direction: column; gap: 5px;">
+            <div class="status-bar">
                 <div>( ) SANGAT SEGERA</div>
                 <div>( ) SEGERA</div>
-            </div> --}}
-            <table class="meta-table" style="width: 100%;">
+            </div>
+
+            <table class="meta-table">
                 <tr>
-                    <td colspan="3" style="font-weight: bold; padding-bottom: 10px;">( ) SANGAT SEGERA</td>
-
-                    <td colspan="3" style="padding-left: 15px; font-weight: bold; padding-bottom: 10px;">( ) SEGERA
-                    </td>
-                </tr>
-
-                <tr>
-                    <td width="17%">Asal Surat</td>
+                    <td width="12%">Asal Surat</td>
                     <td width="2%">:</td>
-                    <td width="46%" class="bg-gray">{{ $surat->pengirim }}</td>
-
-                    <td width="17%" style="padding-left: 15px;">No. Agenda</td>
+                    <td width="45%" class="bg-gray">DISTANDALITU KEMENTERIAN PERDAGANGAN</td>
+                    <td width="12%" style="padding-left: 15px;">No. Agenda</td>
                     <td width="2%">:</td>
-                    <td width="27%" class="bg-gray">{{ $surat->nomor_agenda }}</td>
+                    <td width="27%" class="bg-gray">Exp 113/PST/VI/2026</td>
                 </tr>
                 <tr>
                     <td>Nomor Surat</td>
                     <td>:</td>
-                    <td class="bg-gray">{{ $surat->nomor_surat }}</td>
-                    <td colspan="3"></td>
-                </tr>
-                <tr>
-                    <td>Tgl Surat</td>
-                    <td>:</td>
-                    <td class="bg-gray">{{ \Carbon\Carbon::parse($surat->tanggal)->format('d-m-Y') }}</td>
+                    <td class="bg-gray">PC.00.00/159/PEN.4/SD/06/2026</td>
                     <td colspan="3"></td>
                 </tr>
                 <tr>
                     <td>Tgl Terima</td>
                     <td>:</td>
-                    <td class="bg-gray">{{ \Carbon\Carbon::parse($surat->created_at)->format('d-m-Y') }}</td>
+                    <td class="bg-gray">02-07-2026</td>
                     <td colspan="3"></td>
                 </tr>
             </table>
@@ -243,9 +219,10 @@
             <div class="perihal-section">
                 <div class="perihal-title">Perihal Surat / Undangan</div>
                 <div class="perihal-box">
-                    {{ $surat->perihal }}
+                    UNDANGAN MENGHADIRI BUSINESS MATCHING PRODUSEN PRODUK KARET
                 </div>
             </div>
+
             <table class="tabel-disposisi">
                 <tr>
                     <td colspan="3" class="bg-dark">DITERUSKAN DIREKTUR EKSEKUTIF</td>
@@ -254,24 +231,17 @@
                     <td colspan="3" class="bold">KEPADA :</td>
                 </tr>
                 <tr>
-                    <td width="39%">ASISTEN DIREKTUR <span class="checkbox">( )</span></td>
-                    <td width="28%">DISELESAIKAN <span class="checkbox">( )</span></td>
+                    <td width="33%">ASISTEN DIREKTUR <span class="checkbox">( )</span></td>
+                    <td width="33%">DISELESAIKAN <span class="checkbox">( )</span></td>
                     <td width="34%">MEWAKILI SAYA <span class="checkbox">( )</span></td>
                 </tr>
-            </table>
-            <table class="tabel-disposisi">
-                <tr>
-                    <td width="39%">AASISTEN DIREKTUR 2<span class="checkbox">( )</span></td>
-                    <td width="28%">DI PELAJARI <span class="checkbox">( )</span></td>
-                    <td width="34%">UNTUK DIKETAHUI<span class="checkbox">( )</span></td>
+                <tr class="b-bottom">
+                    <td>ASISTEN DIREKTUR 2 <span class="checkbox">( )</span></td>
+                    <td>DI PELAJARI <span class="checkbox">( )</span></td>
+                    <td>UNTUK DIKETAHUI <span class="checkbox">( )</span></td>
                 </tr>
-            </table>
-            <table class="tabel-disposisi">
                 <tr class="row-catatan">
-                    <td colspan="3"
-                        style="height: 2.5cm !important; min-height: 3.5cm !important; vertical-align: top !important; padding: 10px !important;">
-
-                    </td>
+                    <td colspan="3"></td>
                 </tr>
             </table>
 
@@ -283,12 +253,6 @@
         </div>
 
     </div>
-
-    <script>
-        window.onload = function() {
-            window.print();
-        };
-    </script>
 
 </body>
 
